c443242b7684f0087f88a9699a118958055eccfa
Swastik Roy <swastik.roy@live.com>
2017-11-16 17:54:09 -0700

    Add tokenizer combined for multiple docs

--- common.py
+++ common.py
@@ -6,6 +6,7 @@ from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 lemmatizer = nltk.WordNetLemmatizer()
+from utils import text_extractor as TextExtractor
 # Create p_stemmer of class PorterStemmer
 p_stemmer = PorterStemmer()
 
